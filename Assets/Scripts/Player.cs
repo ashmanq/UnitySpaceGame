b727308b82087d518a93f28d1803956741b35323
Ashir Qureshi <ashirq@outlook.com>
2021-04-05 19:51:59 +0100

    Added limits to where player can move their spaceship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,26 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
 
+    // Cached values
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupMoveBoundaries();
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 
     // Update is called once per frame
@@ -22,8 +38,9 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
