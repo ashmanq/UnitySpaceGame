f474c6be0b4cdedc7e6e095bca2cbfd0a1c9fa47
Ashir Qureshi <ashirq@outlook.com>
2021-04-05 21:24:19 +0100

    Multiple firing now working and laser beams are being destroyed when leaving screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,8 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] float padding = 1f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
- 
+    [SerializeField] float projectileFiringPeriod = 1f;
+
+
     // Cached values
+    Coroutine firingCoroutine;
     float xMin;
     float xMax;
     float yMin;
@@ -43,12 +46,26 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject laser = Instantiate(
-                laserPrefab,
-                transform.position,
-                Quaternion.identity) as GameObject;
-            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            firingCoroutine = StartCoroutine(FireContinously());
         }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinously()
+    {
+        while(true)
+        {
+            GameObject laser = Instantiate(
+             laserPrefab,
+             transform.position,
+             Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+     
     }
 
     private void SetupMoveBoundaries()
