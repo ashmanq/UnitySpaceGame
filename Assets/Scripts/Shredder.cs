34697ed368ae156597797fe0ce4c0e662a8ed602
Ashir Qureshi <ashirq@outlook.com>
2021-04-05 21:25:10 +0100

    Removed collider debug log

--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -6,7 +6,6 @@ public class Shredder : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision!");
         Destroy(collision.gameObject);
     }
 }
