4c69bc52cf1d7badd3d560405309807348e55a88
Ashir Qureshi <ashirq@outlook.com>
2021-04-12 17:28:14 +0100

    Added score and health system

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class ScoreDisplay : MonoBehaviour
@@ -19,9 +18,6 @@ public class ScoreDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (gameSession)
-        //{
-            scoreText.text = gameSession.GetScore().ToString();
-        //}
+        scoreText.text = gameSession.GetScore().ToString();
     }
 }
